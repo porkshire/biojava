6b7c8765d1efc9a73839d0aa031744768950b178
Marcin <porkshire1@gmail.com>
2011-10-24 20:36:51 +0200

    Commit to merge

--- bj/biojava_test/src/biojava_test/Biojava_test.java
+++ bj/biojava_test/src/biojava_test/Biojava_test.java
@@ -34,46 +34,49 @@ public class Biojava_test {
         inputGenerator = new InputGenerator("resources/genbank.txt", InputType.GENBANK);
         //inputGenerator = new InputGenerator("resources/genotype.txt", InputType.FESTA);
         sequences = inputGenerator.readInput();
+        if(sequences.size() > 0) {
+            DNASequence seq;
+            ProteinSequence ps;
+            List<ProteinSequence> list = new ArrayList<ProteinSequence>();
+            for(RichSequence rs : sequences) {
+                 seq = new DNASequence(rs.seqString());
+                 ps = seq.getRNASequence().getProteinSequence();
+                 list.add(ps);
+            }
 
-        DNASequence seq;
-        ProteinSequence ps;
-        List<ProteinSequence> list = new ArrayList<ProteinSequence>();
-        for(RichSequence rs : sequences) {
-             seq = new DNASequence(rs.seqString());
-             ps = seq.getRNASequence().getProteinSequence();
-             list.add(ps);
-        }
-
-        Profile<ProteinSequence, AminoAcidCompound> profile = Alignments.getMultipleSequenceAlignment(list);
-        System.out.println("OK");
-        MultipleSequenceAlignment<ProteinSequence, AminoAcidCompound> msa = new MultipleSequenceAlignment<ProteinSequence, AminoAcidCompound>();
-        for(int i = 0; i < sequences.size(); i++) {
-            ps = new ProteinSequence(profile.getAlignedSequence(i+1).getSequenceAsString());
-            ps.setAccession(new AccessionID(sequences.get(i).getName()));
-            msa.addAlignedSequence(ps);
+            Profile<ProteinSequence, AminoAcidCompound> profile = Alignments.getMultipleSequenceAlignment(list);
             System.out.println("OK");
+            MultipleSequenceAlignment<ProteinSequence, AminoAcidCompound> msa = new MultipleSequenceAlignment<ProteinSequence, AminoAcidCompound>();
+            for(int i = 0; i < sequences.size(); i++) {
+                ps = new ProteinSequence(profile.getAlignedSequence(i+1).getSequenceAsString());
+                ps.setAccession(new AccessionID(sequences.get(i).getName()));
+                msa.addAlignedSequence(ps);
+                System.out.println("OK");
+            }
+            System.out.println("OK" + msa.getSize());
+
+            /*System.out.println("Sekwencje:");
+            for (ProteinSequence ps : msa.getAlignedSequences())
+                System.out.println(ps.getSequenceAsString());*/
+
+            String treeStr = null;
+            TreeConstructor<ProteinSequence, AminoAcidCompound> treeConstructor = new TreeConstructor<ProteinSequence, AminoAcidCompound>(msa, TreeType.NJ, TreeConstructionAlgorithm.PID, new ProgessListenerStub());
+
+            try 
+            {
+                treeConstructor.process();
+                treeStr = treeConstructor.getNewickString(true, true);
+            } 
+            catch (Exception ex) 
+            {
+                Logger.getLogger(Biojava_test.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            System.out.println("OK-END");
+            System.out.println(treeStr);
+        } else {
+            System.out.println("No records found. File does not exists or is in bad format");
         }
-        System.out.println("OK" + msa.getSize());
-
-        /*System.out.println("Sekwencje:");
-        for (ProteinSequence ps : msa.getAlignedSequences())
-            System.out.println(ps.getSequenceAsString());*/
-
-        String treeStr = null;
-        TreeConstructor<ProteinSequence, AminoAcidCompound> treeConstructor = new TreeConstructor<ProteinSequence, AminoAcidCompound>(msa, TreeType.NJ, TreeConstructionAlgorithm.PID, new ProgessListenerStub());
-
-        try 
-        {
-            treeConstructor.process();
-            treeStr = treeConstructor.getNewickString(true, true);
-        } 
-        catch (Exception ex) 
-        {
-            Logger.getLogger(Biojava_test.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        System.out.println("OK-END");
-        System.out.println(treeStr);
         System.exit(0);
     }
 }
---
+++ bj/biojava_test/src/biojava_test/input/Nexus.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biojava_test.input;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.biojava.bio.seq.io.ParseException;
+import org.biojavax.bio.phylo.io.nexus.NexusBlock;
+import org.biojavax.bio.phylo.io.nexus.NexusFile;
+import org.biojavax.bio.phylo.io.nexus.NexusFileBuilder;
+import org.biojavax.bio.phylo.io.nexus.NexusFileFormat;
+import org.biojavax.bio.phylo.io.nexus.TreesBlock;
+import org.biojavax.bio.seq.RichSequence;
+
+/**
+ *
+ * @author porky
+ */
+public class Nexus {
+    
+    private NexusFile nexus;
+
+    public Nexus(String fileName) {
+        try {
+            NexusFileBuilder builder = new NexusFileBuilder();
+            NexusFileFormat.parseFile(builder, new File(fileName));
+            nexus = builder.getNexusFile();
+        } catch (IOException ex) {
+            Logger.getLogger(Nexus.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(Nexus.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public List<RichSequence> readSequences() {
+        List<RichSequence> sequences = new ArrayList<RichSequence>();
+        TreesBlock node = getTreeNode();
+        node.getTree("");
+        return sequences;
+    }
+    
+    TreesBlock getTreeNode() {
+        Iterator it = nexus.blockIterator();
+        NexusBlock block;
+        while(it.hasNext()) {
+            block = (NexusBlock)it.next();
+            if (block.getBlockName().equals("TREES")) {
+                    return (TreesBlock)block;
+            }
+        }
+        return null;
+    }
+}
