7d65d2816a6da562d54c472b86be99fe806bc40c
Lukom <lukom_wro@o2.pl>
2011-10-28 00:07:57 +0200

    Front

--- bj/biojava_test/src/biojava_test/Biojava_test.java
+++ bj/biojava_test/src/biojava_test/Biojava_test.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package biojava_test;
+
 import biojava_test.input.InputGenerator;
 import biojava_test.input.InputType;
 import java.util.ArrayList;
@@ -21,28 +22,27 @@ public class Biojava_test {
 
     private static List<RichSequence> sequences;
     private static InputGenerator inputGenerator;
-    
-    public static void main(String[] args) 
-    {
-        inputGenerator = new InputGenerator("C:\\Users\\DanielWegner\\Desktop\\PhylogenicTree\\biojava\\bj\\biojava_test\\resources\\genbank.txt", InputType.GENBANK);
-        //inputGenerator = new InputGenerator("resources/genotype.txt", InputType.FESTA);
+
+    public static void main(String[] args) {
+//        inputGenerator = new InputGenerator("C:\\Users\\DanielWegner\\Desktop\\PhylogenicTree\\biojava\\bj\\biojava_test\\resources\\genbank.txt", InputType.GENBANK);
+        inputGenerator = new InputGenerator("resources/genotype.txt", InputType.FESTA);
         sequences = inputGenerator.readInput();
 
         DNASequence seq;
         ProteinSequence ps;
         List<ProteinSequence> list = new ArrayList<ProteinSequence>();
-        for(RichSequence rs : sequences) {
-             seq = new DNASequence(rs.seqString());
-             seq.setAccession(new AccessionID(rs.getAccession()));
-             ps = seq.getRNASequence().getProteinSequence();
-             ps.setAccession(new AccessionID(rs.getAccession()));
-             list.add(ps);
+        for (RichSequence rs : sequences) {
+            seq = new DNASequence(rs.seqString());
+            seq.setAccession(new AccessionID(rs.getAccession()));
+            ps = seq.getRNASequence().getProteinSequence();
+            ps.setAccession(new AccessionID(rs.getAccession()));
+            list.add(ps);
         }
 
         /*System.out.println("Sekwencje:");
         for (ProteinSequence ps : msa.getAlignedSequences())
-            System.out.println(ps.getSequenceAsString());*/
-        
+        System.out.println(ps.getSequenceAsString());*/
+
         TreeBuilder t = new TreeBuilder(list);
         String s = t.NeighbourJoining();
         System.out.println(s);
