c14f9bb9b3827d2b1c0076ec2c7eb0b4187ef9ae
wojtiku <wojtiku@gmail.com>
2011-10-28 02:34:10 +0200

    basic visualization

--- bj/front/js/script.js
+++ bj/front/js/script.js
@@ -1,47 +1,25 @@
-/* Author:
-
-*/
-(function(d,w){
-w.onload = function () {
-    var w = 960,
-        h = 2200;
-
-    var cluster = d3.layout.cluster()
-        .size([h, w - 160]);
-
-    var diagonal = d3.svg.diagonal() .projection(function(d) { return [d.y, d.x]; });
-
-    var vis = d3.select("#chart").append("svg:svg")
-        .attr("width", w)
-        .attr("height", h)
-        .append("svg:g")
-        .attr("transform", "translate(40, 0)");
-
-    $('#wyslij').click(function() {
-        var json = eval('('+$('#pole').val()+')');
-        console.log(json);
-        var nodes = cluster.nodes(json);
-
-        var link = vis.selectAll("path.link")
-        .data(cluster.links(nodes))
-        .enter().append("svg:path")
-        .attr("class", "link")
-        .attr("d", diagonal);
-
-        var node = vis.selectAll("g.node")
-            .data(nodes)
-            .enter().append("svg:g")
-            .attr("class", "node")
-            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
-
-            node.append("svg:circle")
-            .attr("r", 4.5);
-
-        node.append("svg:text")
-            .attr("dx", function(d) { return d.children ? -8 : 8; })
-            .attr("dy", 3)
-            .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
-            .text(function(d) { return d.name; });
-    });
-}
-}(document, window));
+$(function(){
+	$("#width, #height").change(function(){
+		$(this)
+			.parent()
+			.parent()
+			.find("strong")
+				.text($(this).val());
+	}).change();
+	
+	/* http://www.jsphylosvg.com/documentation.php */
+	$("#form").submit(function(e){
+		e.preventDefault();
+		
+		paper.clear()
+		$("#chart").empty();
+		var phylocanvas = new Smits.PhyloCanvas(
+			{ newick: $("#data").val() },
+			'chart', 
+			$("#width").val(), 
+			$("#height").val(),
+			$("#type").val()
+		);
+	});
+	
+});
